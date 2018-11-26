d3507470b957f75917c55e6d1be3a508ade7f04c
DavidMutchler <mutchler@rose-hulman.edu>
2018-11-26 16:00:16 -0500

    done

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -57,7 +57,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add another   print   statement below this comment.
 #   It should print any string that you want (but keep it polite and G-rated!)
 #
@@ -72,7 +72,7 @@ print('hi, vibha!')
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
@@ -88,7 +88,7 @@ print(3607 * 34227)
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Look at the list of files in the Project window (to the left).
 #   Note that this file:
 #      m2_todo_and_commit_push.py
